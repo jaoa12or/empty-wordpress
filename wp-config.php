05353944146f989d00389535b2778b74be71666f
alejandro orobio <jhon.orobio@correounivalle.edu.co>
2020-11-26 17:23:32 -0500

    fix: new config

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'mentora' );
+define( 'DB_NAME', 'mgphotostation' );
 
 /** MySQL database username */
 define( 'DB_USER', 'sacmedia' );
